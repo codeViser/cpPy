a3fe5413d37ecd92a9b51099bf842a27a28034e8
codeviser <shubham.note3@gmail.com>
2021-04-22 02:38:29 +0530

    Tweak SegmentTree main for data stream input

--- SegmentTree.py
+++ SegmentTree.py
@@ -82,13 +82,10 @@ class SegmentTree:
 
 
 if __name__ == '__main__':
-    a = [2, 3, 7, 8, 14, 31, 5, 19, 99, 0, -17]
+    num_elem = int(input())
+    arr_in = list(map(int, input().split()))
+    a = arr_in
     segmentTree = SegmentTree(a)
     segmentTree.build()
     print("Built underlying array", segmentTree.get_underlying_array())
     print("Built segment Tree: ", segmentTree.get_segment_tree())
-    print(segmentTree.query(4, 9))
-    segmentTree.update(5, -11)
-    print("Built underlying array", segmentTree.get_underlying_array())
-    print("Built segment Tree: ", segmentTree.get_segment_tree())
-    print(segmentTree.query(4, 9))
